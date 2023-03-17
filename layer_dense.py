6ce2eff153f3b55db6cd67376019272e74ae39d1
leo graziani <graziani.gothe.leo@gmail.com>
2023-03-17 18:23:45 +0100

    Added regularization L1 & L2 for weights update

--- layer_dense.py
+++ layer_dense.py
@@ -27,5 +27,25 @@ class Layer_Dense:
         #Gradients on parameters
         self.dweights = np.dot(self.inputs.T, dvalues)
         self.dbiases = np.sum(dvalues, axis=0,keepdims=True)
+
+        # Gradients on regularization
+        # Weights
+        if self.weight_regularizer_l1 > 0:
+            dL1 = np.ones_like(self.weights)
+            dL1[self.weights < 0] = -1
+            self.dweights += self.weight_regularizer_l1 * dL1
+
+        if self.weight_regularizer_l2 > 0:
+            self.dweights += 2 * self.weight_regularizer_l2 * self.dweights
+
+        # Biases
+        if self.bias_regularizer_l1 > 0:
+            dL1 = np.ones_like(self.biases)
+            dL1[self.biases < 0] = -1
+            self.dbiases += self.bias_regularizer_l1 * dL1
+
+        if self.bias_regularizer_l2 > 0:
+            self.dbiases += 2 * self.bias_regularizer_l2 * self.biases
+
         #Gradients on values
         self.dinputs = np.dot(dvalues,self.weights.T)
--- loss.py
+++ loss.py
@@ -5,6 +5,24 @@ from activations import Activation_Softmax
 #Common loss class
 class Loss:
     #Calculate the data and regularization losses
+    def regularization_loss(self,layer):
+
+        regularization_loss = 0
+
+        if layer.weight_regularizer_l1 > 0:
+            regularization_loss += layer.weight_regularizer_l1 * np.sum(np.abs(layer.weights))
+
+        if layer.weight_regularizer_l2 > 0:
+            regularization_loss += layer.weight_regularizer_l2 * np.sum(layer.weights*layer.weights)
+
+        if layer.bias_regularizer_l1 > 0:
+            regularization_loss += layer.bias_regularizer_l1 * np.sum(np.abs(layer.biases))
+
+        if layer.bias_regularizer_l2 > 0:
+            regularization_loss += layer.bias_regularizer_l2 * np.sum(layer.biases * layer.biases)
+
+        return regularization_loss
+
     #given model output and ground thruth values
     def calculate(self,output,y):
         #calculate sample losses
@@ -89,12 +107,10 @@ class Activation_Softmax_Loss_CategoricalCrossentropy():
         #Turn them into labels
         if len(y_true.shape) == 2:
             y_true = np.argmax(y_true, axis=1)
-        
+
         #copy so we can safely modify
         self.dinputs = dvalues.copy()
         #calculate gradient
         self.dinputs[range(samples),y_true] -= 1
         #Normalize gradients
         self.dinputs = self.dinputs / samples
-        
-        
