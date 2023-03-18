a6b3bfb9aa6d8289acfc632b8989c98368164f51
leo graziani <graziani.gothe.leo@gmail.com>
2023-03-18 17:09:01 +0100

    Added Dropout layer

--- layer_dense.py
+++ layer_dense.py
@@ -49,3 +49,16 @@ class Layer_Dense:
 
         #Gradients on values
         self.dinputs = np.dot(dvalues,self.weights.T)
+
+
+class Layer_Dropout:
+    def __init__(self,rate):
+        self.rate = 1 - rate
+
+    def forward(self,inputs):
+        self.inputs = inputs
+        self.binary_mask = np.random.binomial(1,self.rate, inputs.shape) / self.rate
+        self.output = inputs * self.binary_mask
+
+    def backward(self, dvalues):
+        self.dinputs = dvalues * self.binary_mask
